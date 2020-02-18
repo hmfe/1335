00edb7524807ac8f936a3d7c60f60cb58565386a
Sameh BAHAA <sameh.bahaa@alten.se>
2020-02-18 15:13:06 +0100

    search history css

--- src/search/components/search-history.js
+++ src/search/components/search-history.js
@@ -4,10 +4,10 @@ import './search-history.css';
 class SearchHistory extends React.Component {
     constructor() {
         super();
-        this.onDeleteRecord = this.onDeleteRecord.bind(this);
+        this.onDeleteRecord = this.onDeleteHistoryItem.bind(this);
         this.onClearHistory = this.clearHistory.bind(this);
     }
-    onDeleteRecord(event) {
+    onDeleteHistoryItem(event) {
         this.props.onDeleteRecord(event);
     }
     clearHistory() {
@@ -19,20 +19,20 @@ class SearchHistory extends React.Component {
 
             <article>
                 <section className="hisotryTitle">
-                    <h2 style={{ fontSize: '24px' }}> Search History: </h2>
-                    <button type="button" className="hm-button hm-link" onClick={() => this.clearHistory()}> Clear History </button>
+                    <h2> Search History: </h2>
+                    <button type="button" className="hm-link" onClick={() => this.clearHistory()}> Clear History </button>
                 </section>
                 <hr></hr>
                 <ul>
-                    {this.props.searchHistoryItems.map((item, index) => {
+                    {this.props.searchHistoryItems.map(country => {
                         return (
-                            <React.Fragment key={item.name + index}>
-                                <li style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline' }} key={item.name + index}>
-                                    <h3 style={{ fontSize: "16px", marginRight: 'auto' }}>{item.name}</h3>
-                                    <time style={{ marginRight: '20px', fontSize: '12px', color: 'grey' }} dateTime="item.dateModified.toLocaleTimeString('sv-SE')">
-                                        {item.dateModified.toLocaleTimeString("sv-SE", { dateStyle: 'short', timeStyle : 'short', hour12: 'true' })}
+                            <React.Fragment key={country.alpha2Code}>
+                                <li key={country.alpha2Code}>
+                                    <h3>{country.name}</h3>
+                                    <time dateTime="country.dateModified.toLocaleTimeString('sv-SE')">
+                                        {country.dateModified.toLocaleTimeString("sv-SE", { dateStyle: 'short', timeStyle : 'short', hour12: 'true' })}
                                     </time>
-                                    <button type="button" aria-label="Delete" className="del-btn" onClick={() => this.onDeleteRecord(item)}></button>
+                                    <button type="button" aria-label="Delete" className="del-btn" onClick={() => this.onDeleteHistoryItem(country)}></button>
                                 </li>
                                 <hr></hr>
                             </React.Fragment>
