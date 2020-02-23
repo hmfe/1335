af1fe6e243b92bde3c0eac4442e79696488624a0
Sameh BAHAA <sameh.bahaa@alten.se>
2020-02-23 20:39:08 +0100

    build

---
+++ build/precache-manifest.7be277c707829be3ccaae2b6cdf69780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ef5cf3d9430edff8d1316533ce5bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8745626d43bd003ee3f4",
+    "url": "/static/css/main.72e13f2e.chunk.css"
+  },
+  {
+    "revision": "c0ac7c256193b7863957",
+    "url": "/static/js/2.c84c63a8.chunk.js"
+  },
+  {
+    "revision": "8745626d43bd003ee3f4",
+    "url": "/static/js/main.a6be17f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
